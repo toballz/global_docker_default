1881d4b3c358a10f1e185548187f0c3ebba94250
toballz <42551340+toballz@users.noreply.github.com>
2026-04-15 01:20:22 -0400

    Update build-compose.sh

--- build-compose.sh
+++ build-compose.sh
@@ -3,9 +3,9 @@
 STAGE=${1:-dev}
 
 if [ "$STAGE" = "dev" ]; then
-  STAGE=$STAGE docker-compose -p global_apps -f docker compose.yml -f docker-compose-override.yml up -d --build
+  STAGE=$STAGE docker compose -p global_apps -f docker-compose.yml -f docker-compose-override.yml up -d --build
 elif [ "$STAGE" = "prod" ]; then
-  STAGE=$STAGE docker-compose -p global_apps -f docker compose.yml up -d
+  STAGE=$STAGE docker compose -p global_apps -f docker-compose.yml up -d
 else
   echo "Usage: $0 [dev|prod]"
   exit 1
