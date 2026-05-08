acd623e1d99db0ac11fe0ea0938a8cd1a6756863
King Eli <king@TheKing.kingnet.local>
2026-05-08 19:51:15 -0400

    .

--- build-compose.sh
+++ build-compose.sh
@@ -5,7 +5,7 @@ STAGE=${1:?STAGE is required}
 if [ "$STAGE" = "dev" ]; then
   STAGE=$STAGE docker compose -p global_apps -f docker-compose.yml -f docker-compose-override.yml up -d --build
 elif [ "$STAGE" = "prod" ]; then
-  STAGE=$STAGE docker compose -p global_apps -f docker-compose.yml up -d
+  STAGE=$STAGE docker compose -p global_apps -f docker-compose.yml up -d --build
 else
   echo "Usage: $0 [dev|prod]"
   exit 1
